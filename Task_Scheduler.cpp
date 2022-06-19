9c326f28384dc1d13428c078a59c486889511d6c
Parzivalsg <89161666+Parzivalsg@users.noreply.github.com>
2022-06-19 18:58:26 +0530

    Add files via upload

---
+++ Task_Scheduler.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> count(26, 0);
+        int tasks_size = tasks.size();
+        int max_freq = 0;
+        for (auto &it : tasks) {
+            count[it - 65]++;   // 65 is ascii value of 'A'
+            max_freq = max(max_freq, count[it - 65]);
+        }
+
+        int result = (max_freq - 1) * (n+1);
+        for (auto &it : count) {
+            if (max_freq == it) {
+                result++;
+            }
+        }
+
+        return max(result, tasks_size);
+    }
+};
