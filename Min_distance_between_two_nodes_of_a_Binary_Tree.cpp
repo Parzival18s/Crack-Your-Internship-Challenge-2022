290ae156830c569a7b03b90aa63b26228097c524
Parzivalsg <89161666+Parzivalsg@users.noreply.github.com>
2022-07-07 13:07:27 +0530

    Update Min_distance_between_two_nodes_of_a_Binary_Tree.cpp

--- Min_distance_between_two_nodes_of_a_Binary_Tree.cpp
+++ Min_distance_between_two_nodes_of_a_Binary_Tree.cpp
@@ -18,12 +18,6 @@ class Solution{
         }
         return false;
     }
-    int height(Node* root)
-    {
-        if(!(root))
-            return 0;
-        return 1+max(height(root->left),height(root->right));
-    }
     int help(Node* root,int ptr)
     {
         if(root->data==ptr)
@@ -37,10 +31,12 @@ class Solution{
             return help(root,b);
         if(root->data==b)
             return help(root,a);
-        if((helper(root->right,a) and helper(root->left,b)) 
-        or (helper(root->right,b) and helper(root->left,a)))
+        bool p,q;
+        p=helper(root->right,a);
+        q=helper(root->left,b);
+        if((p and q) or (!p and !q))
             return help(root,a)+help(root,b);
-        if(helper(root->right,a))
+        if(p)
             return findDist(root->right,a,b);
         return findDist(root->left,a,b);
     }
